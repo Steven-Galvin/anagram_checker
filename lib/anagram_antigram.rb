2e75e33a44847bc6497dd0a238a1a22b21dfc72b
Steven Galvin <steven.john.galvin@gmail.com>
2017-04-21 11:05:36 -0700

    test and pass third spec, 'check for palindromes'. change expected outputs in test from the boolean 'true' to the string 'this word is a ___'

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -4,6 +4,15 @@ class String
     first_word = self.split("").sort()
     word2.downcase!()
     second_word = word2.split("").sort()
-    second_word == first_word
+    if second_word == first_word
+      "This word is an anagram!"
+    end
+  end
+
+  define_method(:palindrome_checker?) do
+    reverse_self = self.reverse()
+    if result = self.to_s() == reverse_self.to_s()
+      "This word is a palindrome!"
+    end
   end
 end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -3,9 +3,12 @@ require('anagram_antigram')
 
 describe('String#anagram_checker') do
   it('takes two inputted words and confirms they are anagrams') do |inputted_words|
-    expect("car".anagram_checker?("arc")).to(eq(true))
+    expect("car".anagram_checker?("arc")).to(eq("This word is an anagram!"))
   end
   it('confirms words are anagrams regardless of case') do
-    expect("tAke".anagram_checker?("KaTe")).to(eq(true))
+    expect("tAke".anagram_checker?("KaTe")).to(eq("This word is an anagram!"))
+  end
+  it('checks and confirms palindromes') do
+    expect("racecar".palindrome_checker?()).to(eq("This word is a palindrome!"))
   end
 end
