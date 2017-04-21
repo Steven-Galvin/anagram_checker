6e4940e3d92e6a3c0d9a07582b6e14f09df9eaba
Steven Galvin <steven.john.galvin@gmail.com>
2017-04-21 10:55:05 -0700

    test and pass second spec, 'confirm anagrams regardless of case'

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -5,4 +5,7 @@ describe('String#anagram_checker') do
   it('takes two inputted words and confirms they are anagrams') do |inputted_words|
     expect("car".anagram_checker?("arc")).to(eq(true))
   end
+  it('confirms words are anagrams regardless of case') do
+    expect("tAke".anagram_checker?("KaTe")).to(eq(true))
+  end
 end
